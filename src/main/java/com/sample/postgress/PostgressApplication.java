8d1b175340d31d8ce39864621c8ddeafec5de24d
Rohit Jain <rohitjain.arya2@gmail.com>
2021-08-19 12:39:34 +0530

    Update PostgressApplication.java

--- src/main/java/com/sample/postgress/PostgressApplication.java
+++ src/main/java/com/sample/postgress/PostgressApplication.java
@@ -1,10 +1,22 @@
 package com.sample.postgress;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(scanBasePackages="com.sample")
+@RestController
 public class PostgressApplication {
+
+	@Value("${databasepassword}")
+	private String databasepassword;
+
+	@GetMapping("/message")
+	public String message(){
+		return "Test message>>>>"+databasepassword;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostgressApplication.class, args);
